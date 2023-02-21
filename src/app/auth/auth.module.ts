cafd04186a879fa11e4a9f7fd1efd01e0af3d71f
DESKTOP-Q762QNT\Laura <laura.guerra@col.vueling.com>
2023-02-21 15:31:35 +0100

    Library Added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from '../shared/shared.module';
+
+
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     LoginComponent
+  ],
+  imports: [
+    SharedModule,
+    ButtonModule
   ]
 })
 export class AuthModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { Error404Component } from './error404/error404.component';
-
-
 
 @NgModule({
   declarations: [Error404Component],
-  imports: [
-    CommonModule
-  ],
-  exports: [Error404Component]
+  imports: [CommonModule],
+  exports: [Error404Component, ButtonModule, InputTextModule],
 })
-export class SharedModule { }
+export class SharedModule {}
