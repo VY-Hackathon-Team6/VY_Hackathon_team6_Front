80ed230aa4a591ae315f00b3e4da59724698d5e4
nexus122 <juan-pablo122@hotmail.es>
2023-02-21 23:47:06 +0100

    enviroments vars

---
+++ src/app/enviroments/enviroments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:5000/',
+  inputWorkersUrl: '/api/InputWorkers',
+  outputGroundHandling: '/api/OutputGroundHandling',
+  loginUrl: '/api/User/login',
+};
