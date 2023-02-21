0b85edf56bd50c42ba5af804f45cea6006a11786
Laura-Guerra <93336035+Laura-Guerra@users.noreply.github.com>
2023-02-21 12:44:32 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'VY_Hackathon_Team6_Front';
+}
